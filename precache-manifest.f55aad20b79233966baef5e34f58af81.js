6a1e2338ee527d0a1858730ef62afe8c9d26acc5
Caio Flores <caiopflores@gmail.com>
2018-11-01 00:39:23 -0300

    Updates

---
+++ precache-manifest.f55aad20b79233966baef5e34f58af81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba19f69d2136a9e90006",
+    "url": "/react-easy-kanban/static/js/runtime~main.ba19f69d.js"
+  },
+  {
+    "revision": "9e66068a4cfbf39d1eb3",
+    "url": "/react-easy-kanban/static/js/main.9e66068a.chunk.js"
+  },
+  {
+    "revision": "8b1a8f295d61d573a6ab",
+    "url": "/react-easy-kanban/static/js/1.8b1a8f29.chunk.js"
+  },
+  {
+    "revision": "de40a60bbad813874d4f94891a06bdf3",
+    "url": "/react-easy-kanban/index.html"
+  }
+];
